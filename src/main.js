2cb7f640a08cff5367b342a6c6dd5bb82d3bab36
MultiFruit <MaxutaPavel@gmail.com>
2019-10-15 17:36:24 +0300

    init crm-system

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
+import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBEBCUL2d_ZcTpd__kYX0tikdoYH1yOQ8U",
+  authDomain: "crm-system-multifruit.firebaseapp.com",
+  databaseURL: "https://crm-system-multifruit.firebaseio.com",
+  projectId: "crm-system-multifruit",
+  storageBucket: "",
+  messagingSenderId: "768231734208",
+  appId: "1:768231734208:web:b03c6452b83e0040b72625"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/router.js
+++ src/router.js
@@ -1,25 +1,69 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
+
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+
   base: process.env.BASE_URL,
+
   routes: [
     {
       path: '/',
       name: 'home',
+      meta: { layout: 'main-layout' },
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      meta: { layout: 'empty-layout' },
+      component: () => import ('./views/Login')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/Categories')
+    },
+    {
+      path: '/detail_record',
+      name: 'detail_record',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/DetailRecord')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/History')
+    },
+    {
+      path: '/planning',
+      name: 'planning',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/Planning.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/Profile.vue')
+    },
+    {
+      path: '/record',
+      name: 'record',
+      meta: { layout: 'main-layout' },
+      component: () => import ('./views/Record.vue')
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      meta: { layout: 'empty-layout' },
+      component: () => import ('./views/Registration.vue')
     }
   ]
 })
