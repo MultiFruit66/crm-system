91b684e8111db3c91ece3ab1094f2f3a0166f565
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-15 15:02:52 +0000

    Deploy MultiFruit66/crm-system to github.com/MultiFruit66/crm-system.git:gh-pages

---
+++ precache-manifest.ab85f1b27c83ed38a2e6e3fd4227050d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f1ea9f662143f7378b3",
+    "url": "/crm-system/css/app.4e8e286f.css"
+  },
+  {
+    "revision": "5f1ea9f662143f7378b3",
+    "url": "/crm-system/js/app.39f7ac70.js"
+  },
+  {
+    "revision": "b8b0e09c8b6ee0d34392",
+    "url": "/crm-system/js/chunk-2d0c1056.50ae5d85.js"
+  },
+  {
+    "revision": "e9dad1988ffa4b0e2431",
+    "url": "/crm-system/js/chunk-2d0c9708.d67158ba.js"
+  },
+  {
+    "revision": "20a7d3f383d6e471b2ff",
+    "url": "/crm-system/js/chunk-2d0d05dc.9e1a3651.js"
+  },
+  {
+    "revision": "69bb70a09b651af11170",
+    "url": "/crm-system/js/chunk-2d0d7699.06312a52.js"
+  },
+  {
+    "revision": "28f811cf95ebc7552e6e",
+    "url": "/crm-system/js/chunk-2d217357.e623fe6b.js"
+  },
+  {
+    "revision": "091372aefc142a5d407b",
+    "url": "/crm-system/js/chunk-2d2259e5.c8e4c8fb.js"
+  },
+  {
+    "revision": "da8903f789b222acf526",
+    "url": "/crm-system/js/chunk-5f4ad635.a53c08e1.js"
+  },
+  {
+    "revision": "b01f529382e7812877d7",
+    "url": "/crm-system/js/chunk-a04b3394.f98d0b99.js"
+  },
+  {
+    "revision": "5a5622e51a71c58791ab",
+    "url": "/crm-system/css/chunk-vendors.5abecdc7.css"
+  },
+  {
+    "revision": "5a5622e51a71c58791ab",
+    "url": "/crm-system/js/chunk-vendors.24112da3.js"
+  },
+  {
+    "revision": "340390716b83d7f3e46cdacd0c4f35db",
+    "url": "/crm-system/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-system/robots.txt"
+  }
+];
